73194c9557673a342386b9823274596c7ae21d99
Althea Tyrell <althea.mtyrell@gmail.com>
2023-10-22 12:25:10 -0400

    node-express week 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const campsiteRouter = exporess.Router();
+const campsiteRouter = express.Router();
 
 campsiteRouter.route('/')
 .all((req, res, next) => {
@@ -20,5 +20,27 @@ campsiteRouter.route('/')
 .delete((req, res) => {
     res.end('Deleting all campsites');
 });
+
+campsiteRouter.route('/:campsiteID') 
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+
 
 module.exports = campsiteRouter;
